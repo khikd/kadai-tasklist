68ffa83aae631ed82f8c2b6e990dada7d1aa12e5
Kohei Ikeda <k253.ikeda@gmail.com>
2017-04-28 11:12:19 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,12 +44,13 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to tasks_url 
   end
   
-　private
-　
-　# Strong Parameter
-　def task_params
-　  params.require(:task).permit(:content)
+  private
+
+  # Strong Parameter
+  def   task_params
+    params.require(:task).permit(:content)
+  end
 end
