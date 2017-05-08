6e55f3b591832a6e8e0fb34fcafdd8ad0b0c70c9
Kohei Ikeda <k253.ikeda@gmail.com>
2017-05-08 06:09:08 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.all.page(params[:page])
@@ -39,9 +41,8 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url 
+    redirect_back(fallback_location:  root_path)
   end
   
   private
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @tasklist = current_user.tasklist.build  # form_for 用
+      @tasklists = current_user.tasklist.order('created_at DESC').page(params[:page])
+    end
   end
 end
